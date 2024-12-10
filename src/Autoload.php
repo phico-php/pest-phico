1421d98edadd02d57ee59525dae031b89ec61cb5
indgy <65541311+indgy@users.noreply.github.com>
2024-12-10 14:54:32 +0000

    Copied Pest plugin template

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Pest\Phico;
 
 use Pest\Plugin;
 use PHPUnit\Framework\TestCase;
--- src/Example.php
+++ src/Example.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Pest\Phico;
 
 use PHPUnit\Framework\TestCase;
 
